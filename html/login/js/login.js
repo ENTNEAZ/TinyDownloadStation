588337c1cdcc4cb4f1f509ec7ee899de6119e96f
ENTNEAZ <langyuez@outlook.com>
2023-03-06 18:25:09 +0800

    login screen

---
+++ html/login/js/login.js
@@ -0,0 +1,18 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var data = "username=" + username + "&hashPassword=" + generateHash(password);
+    window.location.href = "https://download.dragon.hotdoge.cn/api/login?" + data;
+}
+
+function generateHash(password) {
+    password =  password + "saltysalt";
+    var hash = 0;
+    if (password.length == 0) return hash;
+    for (i = 0; i < password.length; i++) {
+        char = password.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}

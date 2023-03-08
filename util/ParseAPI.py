7c44166ef01baae4bb6ea96b363ae118384dcb30
ENTNEAZ <langyuez@outlook.com>
2023-03-08 16:41:05 +0800

    return getDownloadList

--- util/ParseAPI.py
+++ util/ParseAPI.py
@@ -5,13 +5,16 @@ import time
 
 import config_test as config
 from . import Log
+from . import UserHelper
+from . import FileHelper
 
 
 def api(http, api: str, query: str):
     query = parse.parse_qs(query)
     try:
         return {
-            'login': login
+            'login': login,
+            'getDownloadList': getDownloadList
         }[api](http, query)
     except KeyError:
         http.send_response(404)
@@ -49,4 +52,34 @@ def login(http, query: dict):
 
 
 def getDownloadList(http, query: dict):
-    ...
+    cookie = http.headers.get('Cookie')
+    if cookie is None:
+        http.send_response(302)
+        http.send_header("Content-type", "text/html")
+        http.send_header("Location", "/login/login.html")
+        http.end_headers()
+        http.wfile.write(b"Login")
+        return
+    cookie = cookie.split(';')
+    realCookie = ''
+    for i in cookie:
+        i = i.strip()  # reduce space
+        if i.startswith('userSessionID='):
+            print(realCookie)
+            realCookie = i.split('=')[1]
+            break
+    # check cookie
+    if not (UserHelper.UserHelper.getInstance().getUserOfCookie(realCookie) is None):
+        # get file list
+        fileList = FileHelper.getFileList()
+        http.send_response(200)
+        http.send_header("Content-type", "application/json")
+        http.end_headers()
+        http.wfile.write(str(fileList).encode())
+    else:
+        http.send_response(302)
+        http.send_header("Content-type", "text/html")
+        http.send_header("Location", "/login/login.html")
+        http.end_headers()
+        http.wfile.write(b"Login")
+        return

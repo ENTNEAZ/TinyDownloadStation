14b9da22805fb4971b53782d83ddb147d7f4ac2a
ENTNEAZ <langyuez@outlook.com>
2023-03-08 16:32:51 +0800

    add speed and rate display

--- html/user/js/upload.js
+++ html/user/js/upload.js
@@ -5,6 +5,8 @@ function upload(){
     formdata.append("file", file);
     //提交文件名
     formdata.append("filename", file.name);
+    var lastTime = 0;
+    var lastSize = 0;
     $.ajax({
         url: "/upload",
         type: "POST",
@@ -15,13 +17,17 @@ function upload(){
             var xhr = new XMLHttpRequest();
             //使用XMLHttpRequest.upload监听上传过程，注册progress事件，打印回调函数中的event事件
             xhr.upload.addEventListener('progress', function (e) {
-                console.log(e);
-                //loaded代表上传了多少
-                //total代表总数为多少
                 var progressRate = (e.loaded / e.total) * 100 + '%';
-
-                //通过设置进度条的宽度达到效果
                 $('.progress > div').css('width', progressRate);
+
+                var nowTime = new Date().getTime();
+                var nowSize = e.loaded;
+                var speed = (nowSize - lastSize) / (nowTime - lastTime) * 1000 / 1024;
+                lastTime = nowTime;
+                lastSize = nowSize;
+                var speedStr = speed > 1024 ? (speed / 1024).toFixed(2) + 'MB/s' : speed.toFixed(2) + 'KB/s';
+                $('.speed').text(speedStr);
+                $(".rate").text((e.loaded / e.total).toFixed(2) * 100 + '%');
             })
 
             return xhr;
